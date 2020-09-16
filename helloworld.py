1a39b93e7308721a30dc3aec4477cd1936fa7a9c
Sonali Kamra <kamra.sonali123@gmail.com>
2020-09-16 13:03:04 -0400

    made corrections

--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,9 @@
 #my name is sonali
 import sys
 def helloworld(num):
-  for i in range(num): 
+  for i in range(num):
   	print('Version 1.0')
   	print('Hello World')
 
-if '__name__' == __main__:
+if __name__ == '__main__':
   helloworld(int(sys.argv[1]))
